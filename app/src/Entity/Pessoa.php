58a085ea4392d8affe7afd6187c356c073b8fa7b
Caio Alexandre de Sousa Ramos <alexandre.caio.ramos@gmail.com>
2025-02-04 18:28:20 -0300

    Autenticação de Usuário

---
+++ app/src/Entity/Pessoa.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'pessoa')]
+class Pessoa
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    private $id;
+
+    #[ORM\Column(type: 'string')]
+    private string $nome;
+
+    #[ORM\Column(type: 'string')]
+    private string $cpf;
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $ativo;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): Pessoa
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getCpf(): string
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(string $cpf): Pessoa
+    {
+        $this->cpf = $cpf;
+        return $this;
+    }
+
+    public function getAtivo(): bool
+    {
+        return $this->ativo;
+    }
+
+    public function setAtivo(bool $ativo): Pessoa
+    {
+        $this->ativo = $ativo;
+        return $this;
+    }
+}
